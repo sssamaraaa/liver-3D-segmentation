8ab84bb078b22c9d7317be361e1d50d02d99f8dd
sssamaraaa <ksenyavozm1407@gmail.com>
2025-11-01 22:44:37 +0500

    correctly vizualization

--- ml/data_preparation.py
+++ ml/data_preparation.py
@@ -1,25 +1,38 @@
 import nibabel as nib
 import matplotlib.pyplot as plt
+from ipywidgets import interact, IntSlider
 
 
-ct_path = "MSD TASK3\Task03_Liver\imagesTr\liver_0.nii.gz"
+ct_path = "MSD TASK3\Task03_Liver\imagesTr\liver_1.nii.gz"
 mask_path = "MSD TASK3\Task03_Liver\labelsTr\liver_0.nii.gz"
 
-def show_image(ct_path, mask_path):
-    ct_img = nib.load(ct_path)
-    mask_img = nib.load(mask_path)
+def interactive_msd_viewer(image_path, mask_path):
+    img = nib.load(image_path)
+    mask = nib.load(mask_path)
+    
+    img_data = img.get_fdata()
+    mask_data = mask.get_fdata()
+    
+    @interact(slice_idx=IntSlider(min=0, max=img_data.shape[2]-1, value=img_data.shape[2]//2))
+    def update_slice(slice_idx):
+        fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+        
+        axes[0].imshow(img_data[:, :, slice_idx], cmap='gray', vmin=-100, vmax=180)
+        axes[0].set_title(f'CT - срез {slice_idx}')
+        
+        axes[1].imshow(mask_data[:, :, slice_idx], cmap='tab10')
+        axes[1].set_title('Маска')
+        
+        axes[2].imshow(img_data[:, :, slice_idx], cmap='gray', vmin=-100, vmax=180)
+        axes[2].imshow(mask_data[:, :, slice_idx], cmap='jet', alpha=0.4)
+        axes[2].set_title('Маска на CT')
+        
+        for ax in axes:
+            ax.axis('off')
+        
+        plt.show()
+        
+        mask_slice = mask_data[:, :, slice_idx]
+        print(f"Вокселей печени в срезе: {mask_slice.sum()} из {mask_slice.size}")
 
-    ct_data = ct_img.get_fdata()
-    mask_data = mask_img.get_fdata()
-
-    z = ct_data.shape[2] // 2
-    ct_slice = ct_data[:, :, z]
-    mask_slice = mask_data[:, :, z]
-    ct_norm = (ct_slice - ct_slice.min()) / (ct_slice.max() - ct_slice.min())
-    mask_scaled = mask_slice * 255
-
-    plt.imshow(ct_norm, cmap="gray")
-    plt.imshow(mask_scaled, cmap="Reds", alpha=0.5) 
-    plt.title(f"Slice {z}")
-    plt.axis("off")
-    plt.show()
+interactive_msd_viewer(ct_path, mask_path)
