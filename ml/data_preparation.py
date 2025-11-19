c36b56440bad6f63359bf8e6d65b73ecd704f156
sssamaraaa <ksenyavozm1407@gmail.com>
2025-11-19 21:58:35 +0500

    added func to visualize infer results

--- ml/data_preparation.py
+++ ml/data_preparation.py
@@ -5,13 +5,14 @@ import random
 
 ct_path = "datasets\MSDTASK3\Task03_Liver\imagesTr\liver_0.nii.gz"
 mask_path = "datasets\MSDTASK3\Task03_Liver\labelsTr\liver_0.nii.gz"
+mask_file = "ml\\test\\test1.nii"
 
 def load_nifti(file_path):
     img = nib.load(file_path)
     data = img.get_fdata()
     return data
 
-def vizualize_msd_sample(ct_file, mask_file, num_slices=5, grid_cols=3):
+def visualize_msd_sample(ct_file, mask_file, num_slices=5, grid_cols=3):
     ct_data = load_nifti(ct_file)
     mask_data = load_nifti(mask_file)
 
@@ -41,4 +42,26 @@ def vizualize_msd_sample(ct_file, mask_file, num_slices=5, grid_cols=3):
     plt.tight_layout()
     plt.show()
 
-vizualize_msd_sample(mask_path, ct_path)
+def visualize_mask(mask_file, num_slices=6, grid_cols=3, cmap="gray"):
+    mask_data = load_nifti(mask_file)
+
+    total_slices = mask_data.shape[2]
+    slice_indices = sorted(random.sample(range(total_slices), num_slices))
+
+    grid_rows = (num_slices + grid_cols - 1) // grid_cols
+    fig, axes = plt.subplots(grid_rows, grid_cols, figsize=(grid_cols*4, grid_rows*4))
+
+    axes = axes.flatten()
+
+    for i, idx in enumerate(slice_indices):
+        axes[i].imshow(mask_data[:, :, idx], cmap=cmap)
+        axes[i].set_title(f"Mask Slice {idx}")
+        axes[i].axis("off")
+
+    for j in range(num_slices, len(axes)):
+        axes[j].axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+visualize_mask(mask_file)
