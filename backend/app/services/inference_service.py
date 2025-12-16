415cb4255c85ee680f76c5e7ac655634678f2f51
sssamaraaa <ksenyavozm1407@gmail.com>
2025-12-17 02:40:55 +0500

    correct output (dict format)

--- backend/app/services/inference_service.py
+++ backend/app/services/inference_service.py
@@ -1,9 +1,12 @@
 from ml.src.inference import inference
 import os
+from datetime import datetime
 
 
 def run_inference(nifti_path, model, device, output_dir):
-    mask_path = os.path.join(output_dir, "mask.nii.gz")
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    mask_filename = f"mask_{timestamp}.nii.gz"
+    mask_path = os.path.join(output_dir, mask_filename)
 
     _ = inference(
         nifti_path,
@@ -12,4 +15,11 @@ def run_inference(nifti_path, model, device, output_dir):
         save_path=mask_path
     )
 
-    return mask_path
+    abs_mask_path = os.path.abspath(mask_path)
+    
+    return {
+        "mask_path": abs_mask_path,
+        "filename": mask_filename,
+        "status": "success",
+        "message": "Segmentation completed"
+    }
