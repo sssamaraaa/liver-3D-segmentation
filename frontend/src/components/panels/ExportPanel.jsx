e27c7c9a5a31ccd37f689f9611ca3689c17e1b6b
sssamaraaa <ksenyavozm1407@gmail.com>
2025-12-26 01:21:02 +0500

    added css

--- frontend/src/components/panels/ExportPanel.jsx
+++ frontend/src/components/panels/ExportPanel.jsx
@@ -1,28 +1,59 @@
-// components/panels/ExportPanel.jsx
+import exportIcon from '../../../assets/Download.svg';
+import save_doc from '../../../assets/save_doc.svg';
+
+
 export default function ExportPanel({ onExport }) {
   return (
     <div className="export-panel">
-      <div className="nav-title">Экспорт</div>
+      <div className="export-title">
+        <img 
+          src={exportIcon} 
+          alt=""
+          className="export-main-icon"
+        />
+        Экспорт
+      </div>
 
       <div className="export-group">
-        <div className="export-label">3D Модель</div>
+        <div className="export-label">
+          <img 
+            src={save_doc} 
+            alt=""
+            className="export-icons"
+          />
+          3D Модель
+        </div>
         <div className="button-group">
-          <button className="export-button" onClick={() => onExport("ply")}>PLY</button>
-          <button className="export-button" onClick={() => onExport("stl")}>STL</button>
+          <button className="export-button-3d" onClick={() => onExport("ply")}>PLY</button>
+          <button className="export-button-3d" onClick={() => onExport("stl")}>STL</button>
         </div>
       </div>
 
       <div className="export-group">
-        <div className="export-label">Маска</div>
+        <div className="export-label">
+          <img 
+            src={save_doc} 
+            alt=""
+            className="export-icons"
+          />
+          Маска
+        </div>
         <div className="button-group">
-          <button className="export-button" onClick={() => onExport("nii")}>NIFTI (.nii)</button>
+          <button className="export-button-mask" onClick={() => onExport("nii")}>NIFTI (.nii)</button>
         </div>
       </div>
 
       <div className="export-group">
-        <div className="export-label">Отчёт</div>
+        <div className="export-label">
+          <img 
+            src={save_doc} 
+            alt=""
+            className="export-icons"
+          />
+          Отчёт
+        </div>
         <div className="button-group">
-          <button className="export-button" onClick={() => onExport("json")}>JSON отчет</button>
+          <button className="export-button-report" onClick={() => onExport("json")}>JSON отчет</button>
         </div>
       </div>
     </div>
