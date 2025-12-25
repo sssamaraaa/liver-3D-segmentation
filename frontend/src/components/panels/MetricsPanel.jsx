70a6e3cc8d93c10884f31327b71a05e74191fe89
sssamaraaa <ksenyavozm1407@gmail.com>
2025-12-25 17:07:37 +0500

    added center of mass

--- frontend/src/components/panels/MetricsPanel.jsx
+++ frontend/src/components/panels/MetricsPanel.jsx
@@ -3,6 +3,7 @@ export default function MetricsPanel({ metrics }) {
     <div className="p-4 border-l border-zinc-800 space-y-4">
       <div>Объём: {metrics.volume_ml} мл</div>
       <div>Площадь: {metrics.surface_mm2} мм²</div>
+      <div>Центр масс: {metrics.center_of_mass}</div>
     </div>
   );
 }
