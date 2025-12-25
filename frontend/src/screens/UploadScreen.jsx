7fd22d1e1851a7c3fa58bc122704809e22c72cea
sssamaraaa <ksenyavozm1407@gmail.com>
2025-12-25 18:02:35 +0500

    new css+icon

--- frontend/src/screens/UploadScreen.jsx
+++ frontend/src/screens/UploadScreen.jsx
@@ -1,5 +1,7 @@
+// UploadScreen.tsx
 import { useAppState } from "../app/appState";
 import { useFileUpload } from "../hooks/useFileUpload";
+import UploadIcon from '../../assets/Insert.svg'
 
 export default function UploadScreen() {
   const { setPhase, setMeshData } = useAppState();
@@ -7,9 +9,12 @@ export default function UploadScreen() {
 
   return (
     <div className="upload-screen">
+      <div className="upload-frame">
+        <h1 className="upload-title">Сегментация КТ печени</h1>
+        <h5 className="upload-title-h5">3D просмотр и анализ</h5>
+      </div>
+
       <div className="upload-content">
-        <h1 className="upload-title">3D Сегментация Печени</h1>
-        
         {isUploading ? (
           <div className="upload-progress">
             <div className="progress-bar">
@@ -23,22 +28,35 @@ export default function UploadScreen() {
             </p>
           </div>
         ) : (
-          <label className="upload-button">
-            Загрузить медицинские данные
-            <input
-              type="file"
-              style={{ display: 'none' }}
-              onChange={(e) => {
-                const file = e.target.files[0];
-                if (file) handleFileUpload(file);
-              }}
-              accept=".nii,.nii.gz, .zip, .tar"
-              disabled={isUploading}
-            />
-          </label>
+          <div className="upload-area">
+            <div className="upload-icon-circle">
+              <img 
+                src={UploadIcon} 
+                alt="Upload icon" 
+                width="64" 
+                height="64"
+              />
+            </div>
+            
+            <label className="upload-button">
+              Загрузить данные
+              <input
+                type="file"
+                style={{ display: 'none' }}
+                onChange={(e) => {
+                  const file = e.target.files?.[0];
+                  if (file) handleFileUpload(file);
+                }}
+                accept=".nii,.nii.gz,.zip,.tar"
+                disabled={isUploading}
+              />
+            </label>
+            
+            <p className="upload-formats">
+              Поддерживаемые форматы: .nii, .nii.gz, .zip, .tar
+            </p>
+          </div>
         )}
-        
-        <p className="upload-hint">Поддерживаются файлы NIfTI (.nii, .nii.gz, .zip, .tar)</p>
       </div>
     </div>
   );
