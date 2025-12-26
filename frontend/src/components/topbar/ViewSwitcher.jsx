790a49fd2c5a79c82944db945b40e578f1820ca7
sssamaraaa <ksenyavozm1407@gmail.com>
2025-12-26 18:49:03 +0500

    added icons

--- frontend/src/components/topbar/ViewSwitcher.jsx
+++ frontend/src/components/topbar/ViewSwitcher.jsx
@@ -1,15 +1,25 @@
 import { useAppState } from "../../app/appState";
+import axes_icon from "../../../assets/axes.svg";
+import threed_icon from "../../../assets/3D.svg";
+
 
 export default function ViewSwitcher() {
   const { view, setView } = useAppState();
+  
   const serverViews = ["3d", "axial", "sagittal", "coronal"];
   
-
   const russianLabels = {
     "3d": "3D",
     "axial": "Аксиальный",
     "sagittal": "Саггитальный",
     "coronal": "Корональный"
+  };
+
+  const svgIcons = {
+    "3d": threed_icon,
+    "axial": axes_icon,
+    "sagittal": axes_icon,
+    "coronal": axes_icon
   };
 
   return (
@@ -20,7 +30,14 @@ export default function ViewSwitcher() {
           onClick={() => setView(v)} 
           className={`switcher-button ${view === v ? 'active' : ''}`}
         >
-          {russianLabels[v]} 
+          <div className="button-content">
+            <img 
+              src={svgIcons[v]} 
+              alt="" 
+              className="switcher-icon"
+            />
+            <span>{russianLabels[v]}</span>
+          </div>
         </button>
       ))}
     </div>
