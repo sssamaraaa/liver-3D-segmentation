c3a0a43adda08f31ab60587d538b28b45546dfb7
sssamaraaa <ksenyavozm1407@gmail.com>
2026-02-16 15:11:08 +0500

    moved metrics to metrics.py

--- ml/src/model.py
+++ ml/src/model.py
@@ -63,33 +63,3 @@ class UNet3D(nn.Module):
         u0 = self.conv_up0(u0)
         out = self.outc(u0)
         return out
-
-def dice_coef(pred, target, eps=1e-6, ignore_empty=True):
-    """Dice, excluding completely empty targets (gt.sum()==0)"""
-    pred = pred.contiguous().view(pred.shape[0], -1) 
-    target = target.contiguous().view(target.shape[0], -1)
-
-    if ignore_empty:
-        valid_mask = target.sum(dim=1) > 0 
-        if valid_mask.any():
-            pred = pred[valid_mask] 
-            target = target[valid_mask]
-        else:
-            return torch.tensor(1.0, device=pred.device) 
-
-    intersection = (pred * target).sum(-1) 
-    denomination = pred.sum(-1) + target.sum(-1) 
-    dice = (2 * intersection) / (denomination + eps) 
-    return dice.mean()
-
-class DiceBCELoss(nn.Module):
-    def __init__(self, weight_bce=0.5):
-        super().__init__()
-        self.bce = nn.BCEWithLogitsLoss()
-        self.weight_bce = weight_bce 
-
-    def forward(self, logits, targets):
-        bce = self.bce(logits, targets) 
-        probs = torch.sigmoid(logits) 
-        dice = dice_coef(probs, targets)
-        return self.weight_bce * bce + (1 - self.weight_bce) * (1 - dice) 
