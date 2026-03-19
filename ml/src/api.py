863d1b5c13b01e7059c46ac834c0620256e4fa27
sssamaraaa <ksenyavozm1407@gmail.com>
2026-03-19 14:19:05 +0500

    cleanup

--- ml/src/api.py
+++ ml/src/api.py
@@ -1,15 +1,34 @@
+import os
+import tempfile
+import torch
+import logging
+from pathlib import Path
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from contextlib import asynccontextmanager
-import tempfile
-from src.model_loader import load_model
-from src.inference import inference
+from ml.src.logging_conf import setup_logging
+from ml.src.utils import load_model_from_checkpoint
+from ml.src.inference import inference
+from dotenv import load_dotenv
 
-MODEL_PATH = "ml/model/unet.pth"
+load_dotenv()
+setup_logging()
+logger = logging.getLogger(__name__)
+
+MODEL_PATH = os.getenv("MODEL_PATH")
+STORAGE_DIR = os.getenv("STORAGE_DIR")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    app.state.model, app.state.model_info = load_model(MODEL_PATH)
+    logger.info(f"Loading model from {MODEL_PATH} on {DEVICE}...")
+
+    app.state.model = load_model_from_checkpoint(MODEL_PATH, device=DEVICE)
+    app.state.device = DEVICE
+    app.state.model.eval()
+
+    logger.info(f"Model loaded successfully on {DEVICE} with dtype {next(app.state.model.parameters()).dtype}")
     yield
+    logger.info("Shutting down ML service...")
 
 app = FastAPI(lifespan=lifespan)
 
@@ -20,13 +39,16 @@ async def infer(file: UploadFile = File(...)):
         nifti_path = tmp.name
 
     try:
-        output_path = nifti_path.replace(".nii.gz", "_mask.nii.gz")
+        os.makedirs(STORAGE_DIR, exist_ok=True)
+        output_filename = f"{Path(file.filename).stem}_mask.nii.gz"
+        output_path = os.path.join(STORAGE_DIR, output_filename)
 
         inference(
             nifti_path,
             model=app.state.model,
-            device=app.state.model_info["device"],
-            save_path=output_path
+            device=app.state.device,
+            save_path=output_path,
+            checkpoint_path=None
         )
 
         return {
@@ -36,3 +58,9 @@ async def infer(file: UploadFile = File(...)):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+    finally:
+        try:
+            os.remove(nifti_path)
+        except:
+            pass
